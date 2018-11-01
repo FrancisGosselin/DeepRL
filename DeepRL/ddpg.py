f5151c5010d496db3c6c9b6078a71e9c9bed9424
FrancisGosselin <37222793+FrancisGosselin@users.noreply.github.com>
2018-10-31 21:32:03 -0400

    resloved bugs in fit_batch and batch sampling

--- DeepRL/ddpg.py
+++ DeepRL/ddpg.py
@@ -25,9 +25,9 @@ class Actor():
         self.action_max_value = action_max_value
         
         self.input_state, self.targets, self.output, self.loss = self.create_net('actor')
-        self.input_state_pred, self.input_action_pred , _ , _ = self.create_net('pred_net_actor')
+        self.input_state_pred, _ , self.output_pred , _ = self.create_net('pred_net_actor')
         self.weights = tf.trainable_variables('actor')
-        self.weights_pred = tf.trainable_variables('pred_net')
+        self.weights_pred = tf.trainable_variables('pred_net_actor')
 
         self.update_pred_net = [ tf.assign(self.weights_pred[i], 
                                         tf.add(  tf.multiply(self.tau, self.weights[i]),
@@ -42,7 +42,7 @@ class Actor():
         
    
     def predict(self, sess, state):
-        return sess.run(self.output, feed_dict = {self.input_state : state} )
+        return sess.run(self.output_pred, feed_dict = {self.input_state_pred : state} )
         
     def fit(self, sess, state, action_targets, action_gradient):
         sess.run(self.optimizer, feed_dict = { self.input_state : state,
@@ -76,10 +76,10 @@ class Critic():
         self.action_max_value = action_max_value
         
         self.input_state, self.input_action, self.targets, self.output, self.loss = self.create_net('critic')
-        self.input_state_pred, self.input_action_pred , _ , _, _ = self.create_net('pred_net_critic')
+        self.input_state_pred, self.input_action_pred , _ , self.output_pred , _ = self.create_net('pred_net_critic')
         self.weights = tf.trainable_variables('critic')
-        self.weights_pred = tf.trainable_variables('pred_net')
-        
+        self.weights_pred = tf.trainable_variables('pred_net_critic')
+            
         self.update_pred_net = [ tf.assign(self.weights_pred[i], 
                                         tf.add(  tf.multiply(self.tau,self.weights[i]),
                                                  tf.multiply(1.0-self.tau, self.weights_pred[i]) ))
@@ -88,8 +88,8 @@ class Critic():
         self.action_gradient = tf.gradients(self.loss, self.input_action)
    
     def predict(self, sess, state, action):
-        return sess.run(self.output, feed_dict = { self.input_state : state,
-                                                self.input_action : action})
+        return sess.run(self.output_pred, feed_dict = { self.input_state_pred : state,
+                                                        self.input_action_pred : action})
         
     def fit(self, sess, state, action, Q_target):
         sess.run(self.optimizer, feed_dict = { self.input_state : state,
@@ -110,10 +110,14 @@ class Critic():
         targets = tf.placeholder(dtype='float', shape=[None,1], name='targets')
         
         with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE):
-            n1 = fully_connected(inputs=inputs_state, num_outputs=32, activation_fn=relu)
-            n2 = fully_connected(inputs=n1, num_outputs=1, activation_fn=relu)
-            actions = fully_connected(inputs=inputs_action, num_outputs=1, activation_fn=relu)
-            output = relu(tf.add(actions,n2))
+            n1 = fully_connected(inputs=inputs_state, num_outputs=32, activation_fn=relu, scope='l1')
+            n2 = fully_connected(inputs=n1, num_outputs=32, activation_fn=relu, scope='l2')
+            actions = fully_connected(inputs=inputs_action, num_outputs=32, activation_fn=relu, scope='action_layer')
+            
+            n2_added = tf.add(actions,n2)
+            
+            output = fully_connected(inputs=n2_added, num_outputs=self.action_dim, activation_fn=None, scope='n3')
+           
             loss = mean_squared_error(targets, output)
         
          
@@ -121,26 +125,29 @@ class Critic():
     
 def fit_batch(sess, batch, actor, critic, discount):
     Q_targets = []
-    action_gradients = []
-    for i in range(len(batch)):
-        next_state, reward, action, state, done = batch[i]
-        next_action = actor.predict(sess,next_state)
+    states = batch[3].tolist()
+    actions = batch[2].tolist()
+    next_states = batch[0].tolist()
+    Q_max_predictions = critic.predict(sess, next_states, actor.predict(sess, states))
+    for i in range(len(batch)-1):
+        done = batch[4][i]
+        reward = batch[1][i]
         Q_target = reward 
         if(not done):
-            Q_target += discount*critic.predict(sess, next_state, next_action)[0][0]
-        Q_targets.append([Q_target])
-        action_gradient = critic.get_action_gradient(sess, state, action, [[Q_target]])
-        action_gradients.append(action_gradient[0][0])
-    actions = np.array(batch)[:,2][0]
-    states = np.array(batch)[:,3][0]
+            Q_target += discount*Q_max_predictions[i][0]
+        Q_targets.append([Q_target])  
+    action_gradients = critic.get_action_gradient(sess, states, actions, Q_targets)
     critic.fit(sess, states, actions, Q_targets)
-    actor.fit(sess, states, actions, action_gradients)
-        
+    actor.fit(sess, states, actions, action_gradients[0])
+
+def get_batch(memory, batch_size):
+    batch = random.sample(memory,batch_size)
+    return np.array(batch).T
     
 def train():
     with tf.Session() as sess:
         episodes = 1000
-        batch_size = 32
+        batch_size = 4
         discount_factor = 0.99
 
         env = gym.make('MountainCarContinuous-v0')
@@ -148,7 +155,7 @@ def train():
         state_dim = len(env.reset())
         action_max = env.action_space.high[0]
 
-        memory = deque(maxlen=2000)
+        memory = deque(maxlen=20000)
         actor = Actor(state_dim, action_dim, action_max, batch_size)
         critic = Critic(state_dim, action_dim, action_max)
         action_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(action_dim))
@@ -156,21 +163,20 @@ def train():
 
         for episode in range(episodes):
             done = False
-            state = np.reshape(env.reset(), [1,state_dim])
+            state = env.reset()
             total_reward = 0
             while(not done):
                 action = actor.predict(sess, np.reshape(state, (1, state_dim))) + action_noise()
                 next_state, reward, done, info = env.step(action)
-                next_state = np.reshape(next_state,[1,state_dim])
-                memory.append([next_state,reward, action, state, done])
+                memory.append([np.squeeze(next_state),reward, action[0], np.squeeze(state), done])
                 total_reward += reward
                 env.render()
-                if len(memory) > 32:
-                    batch = random.sample(memory,batch_size)
-                    fit_batch(sess, batch, actor, critic, discount_factor)
+                if len(memory) > batch_size:
+                    batch = get_batch(memory, batch_size)
+                    fit_batch(sess, list(batch), actor, critic, discount_factor)
                     actor.update_net(sess)
                     critic.update_net(sess)
-                    
+                state = next_state    
             print('episode: %d ... total reward: %d' %(episode,total_reward))
                     
 train()
